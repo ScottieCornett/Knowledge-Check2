a47ec5ac67fa96eaccf2d49af4b5e165c2093eaf
Scottie Cornett <scottie.cornett@gmail.com>
2022-10-30 20:29:11 -0400

    initial commit

---
+++ KC2/KC2/Chevrolet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KC2
+{
+    public class Chevrolet
+    {
+        public string Color { get; set; }
+        public int TopSpeed { get; set; }
+        public string ModelType { get; set; }
+    }
+}
---
+++ KC2/KC2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace KC2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<Corvette>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var myCorvette = new Corvette();
+
+                Console.WriteLine("Enter the color for your Corvette: ");
+                myCorvette.Color = Console.ReadLine();
+                Console.WriteLine("Enter the top speed for your Corvette");
+                myCorvette.TopSpeed = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the model type of your Corvette");
+                myCorvette.ModelType = Console.ReadLine();
+
+
+
+                recordList.Add(myCorvette);
+            }
+
+            // Print out the list of records using Console.WriteLine()
+            foreach(var record in recordList)
+                Console.WriteLine(record.ToString());
+
+        }
+    }
+}
